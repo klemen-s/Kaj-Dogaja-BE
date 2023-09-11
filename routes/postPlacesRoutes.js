30d43257357e5748ceeb74eb658b3c2fd4d8261c
klemenslatnar <klemen.slatnar06@gmail.com>
2023-09-11 17:50:07 +0200

    Implemented all post-place routes

--- routes/postPlacesRoutes.js
+++ routes/postPlacesRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const isAuth = require("../authentication/isAuth");
 const Place = require("../models/Place");
 
 router.post("/", async (req, res) => {
@@ -10,20 +9,63 @@ router.post("/", async (req, res) => {
   }
 
   try {
-    console.log(req.body.place);
-    // const place = await Place.findOne({ placeName: req.placeName });
+    const placeName = req.body.place.placeName;
+    const coordinates = req.body.place.coordinates;
+    const imageUrl = req.body.place.imageUrl;
+    const description = req.body.place.description;
+    const region = req.body.place.region;
+    const tripType = req.body.place.tripType;
+    const budget = req.body.place.budget;
+    const attractions = req.body.place.attractions;
 
-    res.json({ message: "Tu lahko objavljaš nove izlete." });
+    if (
+      !placeName ||
+      !coordinates ||
+      !imageUrl ||
+      !description ||
+      !region ||
+      !tripType ||
+      !budget ||
+      attractions.length < 2
+    ) {
+      return res.json({ message: "Manjkajo podatki.", isError: true });
+    }
+
+    const newPlace = {
+      placeName: placeName,
+      coordinates: coordinates,
+      imageUrl: imageUrl,
+      description: description,
+      region: region,
+      tripType: tripType,
+      budget: budget,
+      attractions: attractions,
+    };
+
+    const place = new Place({ ...newPlace });
+
+    await place.save();
+
+    res.json({
+      message: "Izlet je bil uspešno shranjen v podatkovno bazo.",
+      isSuccess: true,
+    });
   } catch (error) {
     return error;
   }
 });
 
 router.post("/placeName", async (req, res) => {
-  try {
-    const placeName = await Place.findOne({ placeName: req.body.placeName });
+  if (!req.isAuth) {
+    return res.json({ errorMsg: "Nimaš privilegijev za to dejanje." });
+  }
 
-    if (placeName) {
+  try {
+    const placeName = req.body.placeName;
+
+    const placeNameExists = await Place.findOne({ placeName: placeName });
+
+    if (placeNameExists) {
       return res.json({
         message: "Ta izlet že obstaja. Ustvari nov izlet.",
         isError: true,
@@ -31,7 +73,7 @@ router.post("/placeName", async (req, res) => {
     }
 
     res.json({
-      message: "Ta izlet še ne obstaja, lahko ga ustvariš.",
+      message: "Izlet še ne obstaja, lahko ga ustvariš.",
       isError: false,
     });
   } catch (error) {
